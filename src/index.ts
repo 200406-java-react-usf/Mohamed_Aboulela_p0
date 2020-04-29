67690d05e055a20d04734e0ced82c938f8e14a5e
mohamedaymantaha <maa16f@my.fsu.edu>
2020-04-29 10:42:27 -0500

    Update index.ts

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import fs from 'fs';
+import morgan from 'morgan';
+import path from 'path';
+
+import { UserRouter } from './routers/user-router';
+//import { PostRouter } from './routers/post-router';
+import { AuthRouter } from './routers/auth-router';
+import { sessionMiddleware } from './middleware/session-middleware';
+import { corsFilter } from './middleware/cors-filter';
+
+const app = express();
+
+// logging configuration
+fs.mkdir(`${__dirname}/logs`, () => {});
+const logStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' });
+app.use(morgan('combined', { stream: logStream }));
+
+app.use('/', express.json());
+app.use(sessionMiddleware);
+app.use(corsFilter);
+
+
+app.use('/users', UserRouter);
+//app.use('/posts', PostRouter);
+app.use('/auth', AuthRouter);
+
+
+app.listen(8080, () => {
+    console.log(`Application running and listening at: http://localhost:8080`);
+});
