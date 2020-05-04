9b0b5e31dbaf1c12544ff7ba631aa5e0a83bbcf7
mohamedaymantaha <maa16f@my.fsu.edu>
2020-05-04 10:41:10 -0500

    Update astrology-router.ts

--- src/routers/astrology-router.ts
+++ src/routers/astrology-router.ts
@@ -6,21 +6,21 @@ export const AstrologyRouter = express.Router();
 
 const astrologyService = AppConfig.astrologyService;
 
-AstrologyRouter.get('/', async (req, resp) => {
-    try {
-        let payload = await astrologyService.getAll();
-        return resp.status(200).json(payload);
-    } catch (e) {
-        return resp.status(404).json(e).send();
-    }
-});
+// AstrologyRouter.get('/', async (req, resp) => {
+//     try {
+//         let payload = await astrologyService.getAll();
+//         return resp.status(200).json(payload);
+//     } catch (e) {
+//         return resp.status(404).json(e).send();
+//     }
+// });
 
-AstrologyRouter.get('/:id', async (req, resp) => {
-    const id = +req.params.id; // the plus sign is to type coerce id into a number
-    try {
-        let payload = await astrologyService.getById(id);
-        return resp.status(200).json(payload);
-    } catch (e) {
-        return resp.status(404).json(e).send();
-    }
-});
+// AstrologyRouter.get('/:id', async (req, resp) => {
+//     const id = +req.params.id; // the plus sign is to type coerce id into a number
+//     try {
+//         let payload = await astrologyService.getById(id);
+//         return resp.status(200).json(payload);
+//     } catch (e) {
+//         return resp.status(404).json(e).send();
+//     }
+// });
