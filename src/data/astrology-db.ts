1d2b4a3fd3113f3c6598b686f58c085778fcd378
mohamedaymantaha <maa16f@my.fsu.edu>
2020-05-05 21:44:03 -0500

    Update astrology-db.ts

--- src/data/astrology-db.ts
+++ src/data/astrology-db.ts
@@ -1,15 +1,6 @@
-//import { Post } from '../models/post';
 import { Astrology } from '../models/astrology';
 
-// challenge: replace this with id generator function
 let id = 1;
-// export default [
-//     new Post(id++, 'hello world!', 'This is my first post! Huzzah!', 1),
-//     new Post(id++, 'this is cool', 'NodeJS is so awesome! It uses the same JS engine as Google Chrome: V8', 1),
-//     new Post(id++, 'no limits', 'NodeJS is a runtime environment that lets JS run outside of the browser!', 2),
-//     new Post(id++, 'in the cloud', 'the ability to do run JS outside the browser has made NodeJS popular for "serverless" applications', 3),
-//     new Post(id++, 'single-threaded?', 'i\'ve heard that JS is single threaded, is that true?', 4)
-// ];
 
 export default [
     new Astrology(id++, 'Aquarius', 'Although they can easily adapt to the energy that surrounds them, Aquarius-born have a deep need to be some time alone and away from everything, in order to restore power. People born under the Aquarius sign, look at the world as a place full of possibilities. Aquarius is an air sign, and as such, uses his mind at every opportunity. If there is no mental stimulation, they are bored and lack a motivation to achieve the best result. The ruling planet of Aquarius, Uranus has a timid, abrupt and sometimes aggressive nature, but it also gives Aquarius visionary quality. They are capable of perceiving the future and they know exactly what they want to be doing five or ten years from now. Uranus also gave them the power of quick and easy transformation, so they are known as thinkers, progressives and humanists. They feel good in a group or a community, so they constantly strive to be surrounded by other people. The biggest problem for Aquarius-born is the feeling that they are limited or constrained. Because of the desire for freedom and equality for all, they will always strive to ensure freedom of speech and movement. Aquarius-born have a reputation for being cold and insensitive persons, but this is just their defence mechanism against premature intimacy. They need to learn to trust others and express their emotions in a healthy way.', 1),
