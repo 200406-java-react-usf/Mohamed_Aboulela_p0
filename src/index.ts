e823a5e900b7bb0ee52e2ab461f5c50782e69ffb
mohamedaymantaha <maa16f@my.fsu.edu>
2020-05-05 21:40:39 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,37 @@ import express from 'express';
 import fs from 'fs';
 import morgan from 'morgan';
 import path from 'path';
+import dotenv from 'dotenv';
+
 
 import { UserRouter } from './routers/user-router';
 import { AstrologyRouter } from './routers/astrology-router';
 import { AuthRouter } from './routers/auth-router';
 import { sessionMiddleware } from './middleware/session-middleware';
 import { corsFilter } from './middleware/cors-filter';
+import { Pool } from 'pg';
+
+dotenv.config();
+
+export const connectionPool: Pool = new Pool({
+	host: process.env['DB_HOST'],
+	port: +process.env['DB_PORT'],
+	database: process.env['DB_NAME'],
+	user: process.env['DB_USERNAME'],
+	password: process.env['DB_PASSWORD'],
+	max: 5
+});
 
 const app = express();
 
 // logging configuration
 fs.mkdir(`${__dirname}/logs`, () => {});
 const logStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), { flags: 'a' });
-app.use(morgan('combined', { stream: logStream }));
 
+app.use(morgan('combined', { stream: logStream }));
 app.use('/', express.json());
 app.use(sessionMiddleware);
 app.use(corsFilter);
-
-
 app.use('/users', UserRouter);
 app.use('/signs', AstrologyRouter);
 app.use('/auth', AuthRouter);
