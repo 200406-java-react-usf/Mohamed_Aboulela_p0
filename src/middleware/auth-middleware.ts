db48f8d5c1c765bd3c95cf2ac3fe5cea9ca7208a
mohamedaymantaha <maa16f@my.fsu.edu>
2020-05-07 13:46:49 -0500

    Internal Server Error fix

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -4,11 +4,11 @@ import { AuthenticationError, AuthorizationError } from "../errors/errors";
 export const adminGuard = (req: Request, resp: Response, next) => {
 
     if (!req.session.principal) {
-        resp.sendStatus(401).send(new AuthenticationError('No session found! Please login.'));
+        resp.status(401).send(new AuthenticationError('No session found! Please login.'));
     } else if (req.session.principal.role === 'Admin') {
         next();
     } else {
-        resp.sendStatus(403).send(new AuthorizationError());
+        resp.status(403).send(new AuthorizationError());
     }
 
 }
